badff0d7700337bd435fc58a64ba8c809b49b48b
bVill <bryanisaacvillagomezmedrano96@gmail.com>
2020-05-09 14:02:52 -0500

    Ya lee n enteros de la consola

--- main.c
+++ main.c
@@ -9,7 +9,16 @@
 
 int main(int argc, char **argv)
 {
-	printf("Hola\n");
+		int n, arreglo[100],indice;
+	printf("Cuantos numeros se van a sumar?:");
+	scanf("%d",&n);
+	for(indice =0; indice <n; indice++)
+		{
+			printf("Cual es el numero n?: %d",indice);
+			scanf("%d",&(arreglo[indice]));
+			printf("%d/n", arreglo[indice]);
+		}
+	
 	return 0;
 }
 
